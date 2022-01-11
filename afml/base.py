88851ab38518b9a3dcbc0d8546ad84e94539c8c0
AlbertoVelascoMata <Alberto.Velasco@uclm.es>
2022-01-11 18:19:15 +0100

    hotfix: crash on empty params

--- afml/base.py
+++ afml/base.py
@@ -2,7 +2,7 @@
 class BaseObject:
     def __init__(self, name=None, params={}):
         self._name = name
-        self._params = params
+        self._params = params if params else {}
 
     @property
     def name(self):
